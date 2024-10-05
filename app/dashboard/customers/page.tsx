53a454d7adcf3a26330ce37492a1ee2db2692b28
Jezebel1990 <jezebelguedes@gmail.com>
2024-10-04 23:20:15 -0300

    Add Customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,26 @@
 import { Metadata } from 'next';
+import { lusitana } from '@/app/ui/fonts';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import CustomersTable from '@/app/ui/customers/table';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-    return <p>Customers Page</p>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+
+  };
+  }) {
+    const query = searchParams?.query || '';
+    const customers = await fetchFilteredCustomers(query);
+  
+  return (
+    <div className={`${lusitana.className} w-full`}>
+      <CustomersTable  customers={customers} />
+    </div>
+  );
   }
